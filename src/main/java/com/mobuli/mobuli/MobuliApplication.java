55bf0ec9537031cdb1a898652d6f88666e9f23a6
hmackowski <hmackowski@gmail.com>
2024-02-19 19:22:26 -0500

    first commit

---
+++ src/main/java/com/mobuli/mobuli/MobuliApplication.java
@@ -0,0 +1,13 @@
+package com.mobuli.mobuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobuliApplication.class, args);
+	}
+
+}
